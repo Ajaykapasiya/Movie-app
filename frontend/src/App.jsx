1de6d3400721eeb03fb2443a7a6a17a99c0bf509
ajay <ajaykumar290904@gmail.com>
2025-06-08 11:12:20 +0530

    feat: replace MovieCard with Home component in App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,12 @@
 import "./App.css";
 import MovieCard from "./components/movieCard";
+import Home from "./pages/Home";
 
 function App() {
   const movienumber = 1;
   return (
     <>
-      {movienumber === 1 ? (
-        <MovieCard movie={{ title: "Welcome", release_date: "2016" }} />
-      ) : (
-        <MovieCard movie={{ title: "MI", release_date: "2025" }} />
-      )}
+      <Home/>
     </>
   );
 }
