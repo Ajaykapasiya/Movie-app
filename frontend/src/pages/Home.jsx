93ff189a6bd36b272b8571a8f811d490453781ed
ajay <ajaykumar290904@gmail.com>
2025-06-09 14:47:57 +0530

    feat: add search functionality to Home component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,16 +1,31 @@
 import React from "react";
 import MovieCard from "../components/movieCard";
+import { useState } from "react";
+
 
 function Home() {
+    const [searchQuery , setSearchQuery] = useState("")
   const movies = [
     { id: 1, title: "John Wick", release_date: "2020" },
     { id: 2, title: "Welcome", release_date: "2014" },
-    { id: 3, title: "MI-1", release_date: "1996" }, 
+    { id: 3, title: "MI-1", release_date: "1996" },
     { id: 4, title: "Atonment", release_date: "2019" },
   ];
 
+  const handleSearch = () => {};
+
   return (
     <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search movies..."
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button type="submit" className="search-button">Search</button>
+      </form>
       <div className="movies-grid">
         {movies.map((movies) => (
           <MovieCard movie={movies} key={movies.id} />
